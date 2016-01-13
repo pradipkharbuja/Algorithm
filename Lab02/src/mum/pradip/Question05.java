d25f5e3cecdcca018f692a92dfc17f51411477fe
Pradip Kharbuja <pradipkharbuja@gmail.com>
2016-01-13 00:31:01 -0600

    Lab 02 Part 1 completed

--- Lab02/src/mum/pradip/Question05.java
+++ Lab02/src/mum/pradip/Question05.java
@@ -6,41 +6,46 @@ import java.util.Set;
 
 //PowerSet
 /*
- * Algorithm: PowerSet(X)
- Input: A list X of elements
- Output: A list P consisting of all subsets of X – elements of P are Sets
-
- P ← new list
- S ← new Set //S is the empty set
- P.add(S) //P is now the set { S }
- T ← new Set
- while (!X.isEmpty() ) do
- f ← X.removeFirst()
- for each x in P do
- T ← x U {f} // T is the set containing f & all elements of x
- P.add(T)
- return P
+ Algorithm: PowerSet(X)
+	Input: A list X of elements
+	Output: A list P consisting of all subsets of X – elements of P are Sets
+	
+	P ← new list
+	S ← new Set //S is the empty set
+	P.add(S) //P is now the set { S }
+	T ← new Set
+	while (!X.isEmpty() ) do
+		f ← X.removeFirst()
+		for each x in P do
+			T ← x U {f} // T is the set containing f & all elements of x
+			P.add(T)
+	return P
  */
 
 public class Question05 {
 
 	public static ArrayList<Set<Integer>> powerSet(ArrayList<Integer> x) {
+		
 		ArrayList<Set<Integer>> p = new ArrayList<>();
-
 		Set<Integer> s = new HashSet<Integer>();
 
 		p.add(s);
 
-		Set<Integer> t = new HashSet<Integer>();
-
 		while (!x.isEmpty()) {
 			int f = x.remove(0);
-
-			for (Set<Integer> temp : p) {
-				temp.add(f);
-				t.addAll(temp);				
+	
+			ArrayList<Set<Integer>> tempList = new ArrayList<>();
+			
+			for (Set<Integer> setTemp : p) {
+				tempList.add(setTemp);
 			}
-			p.add(t);
+			
+			for (Set<Integer> temp : tempList) {
+				Set<Integer> S = new HashSet<>();				
+				S.addAll(temp);
+				S.add(f);
+				p.add(S);
+			}		
 		}
 		return p;
 
